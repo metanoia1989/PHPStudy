c381c88e6bae74ce918cf55d83f80fcd427aef89
Adam Smith <sogaxili@gmail.com>
2020-04-23 15:40:19 +0800

    删除视图编译文件

--- Laravel/lara-handle/storage/framework/views/edc5a281fb459a0157493277c4cf98c47ab87da7.php
+++
@@ -1,4 +0,0 @@
-<h3>在视图中显示学生信息：</h3>
-<div>学生ID：<?php echo e($data['id']); ?>；</div>
-<div>学生name：<?php echo e($data['name']); ?>；</div>
-<div>学生age：<?php echo e($data['age']); ?>；</div><?php /**PATH E:\WorkSpace\PHPStudy\Laravel\lara-handle\resource\views/welcome.blade.php ENDPATH**/ ?>
