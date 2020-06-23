4fe03091510a7df05b920bed239f21f8804fad69
metanoia1989 <sogaxili@gmail.com>
2020-06-23 23:57:02 +0800

    添加room房间

---
+++ Laravel/first-project/app/Services/WebSocket/Rooms/RedisRoom.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services\Websocket\Rooms;
+
+use Illuminate\Support\Arr;
+use Predis\Client as RedisClient;
+
+/**
+ * Redis 作为存储媒介
+ */
+class RedisRoom implements RoomContract
+{
+    /**
+     * @var \Predis\Client
+     */
+    protected $redis;
+
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * @var string
+     */
+    protected $prefix = 'swoole:';
+
+    /**
+     * RedisRoom constructor
+     *
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function prepare(RedisClient $redis = null): RoomContract
+    {
+        $this->setRedis($redis);
+        $this->setPrefix();
+        $this->cleanRooms();
+
+        return $this;
+    }
+
+    public function setRedis(?RedisClient $redis = null)
+    {
+        if (!$redis) {
+            $server = Arr::get($this->config, 'server', []);
+            $options = Arr::get($this->config, 'options', []);
+
+            // forbid setting prefix from options
+            if (Arr::has($options, 'prefix')) {
+                $options = Arr::except($options, 'prefix');
+            }
+            $redis = new RedisClient($server, $options);
+        }
+
+        $this->redis = $redis;
+    }
+}
