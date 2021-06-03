b8a757d2749eb66d770996a489796d9a2381347c
刘深海 <m15171641694@163.com>
2021-06-03 14:53:35 +0800

    feat: 添加dom节点操作

--- Base/DOM/DomDocument.php
+++ Base/DOM/DomDocument.php
@@ -1,6 +1,7 @@
 <?php
 require_once "curl.php";
 
+$baseUrl = "https://www.lianquan.org/";
 $url = "https://www.lianquan.org/ContractPlus_SeriesInfo?guid=F138D3BCD709FB3D696B64964376211BE566C1C241448A920BBC9E7780442A77DBD9A035A02A3FCA1225E9B60B589C25";
 $html =  httpGet($url);
 
@@ -11,4 +12,28 @@ $dom->loadHTML($html);
 // 访问 document element 元素
 $documentElement = $dom->documentElement;
 
-echo json_encode($html);
+// 根据ID获取元素内容
+$header = $dom->getElementById('header'); // DOMElement
+$child_elements = $header->getElementsByTagName('li'); // DOMNodeList
+$row_count = $child_elements->length - 1;
+$li_content = trim($child_elements->item(1)->nodeValue);
+echo $li_content."\n";
+
+// 通过标签名来选择
+// Both the DOMDocument and DOMElement classes have the method getElementsByTagName()
+$h4s = $dom->getElementsByTagName('h4');
+foreach ($h4s as  $h4) {
+    echo trim($h4->textContent)."\n";
+}
+
+// 使用 xpath 来查找节点
+// 使用 getAttribute() 方法来获取，使用 hasAttribute() 来判断
+$xpath = new DOMXPath($dom);
+// $items = $xpath->query('//*[@id="publishingagency"]//div[contains(@class,"item")]');
+$items = $xpath->query('//*[@id="publishingagency"]//div[@class="item"]/a');
+echo "找到了{$items->length}个元素！\n";
+foreach($items as $item) {
+    echo trim($item->textContent)."\n";
+    $url = $baseUrl.$item->getAttribute("href");
+    echo $url."\n";
+}
--- Base/DOM/curl.php
+++ Base/DOM/curl.php
@@ -7,8 +7,23 @@ function httpGet($url)
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 //  curl_setopt($ch,CURLOPT_HEADER, false); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
+	$header = [
+		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+	];
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
     $output=curl_exec($ch);
+
+	if (curl_errno($ch)) {
+        return curl_error($ch);
+	} else {
+		$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if (200 !== $httpStatusCode) {
+            return $output;
+		}
+	}
 
     curl_close($ch);
     return $output;
