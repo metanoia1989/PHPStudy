fbe590036df42e5371ffcb87dd88ac416520020b
AdamSmith <m15171641694@163.com>
2021-06-11 17:10:00 +0800

    chore: 添加Laravel集成优化

---
+++ Base/Extension/FirstExt/src/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Metanoia1989\FirstExt;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = true; 
+
+    public function register()
+    {
+        $this->app->singleton(Weather::class, function() {
+            return new Weather(config('services.weather.key'));
+        });
+
+        $this->app->alias(Weather::class, 'weather');
+    }
+
+    public function provides()
+    {
+        return [Weather::class, 'weather'];
+    }
+}
