637b054d96f1c6d7dc0e218a8ff2bc60d875ec7c
metanoia1989 <sogaxili@gmail.com>
2021-08-22 17:25:51 +0800

    feat: 添加session编码解码

---
+++ Base/session/class.SessionHelper.php
@@ -0,0 +1,75 @@
+<?php
+class SessionHelper {
+    /**
+     * @see http://php.net/manual/en/function.session-encode.php#76425
+     * @param array $array
+     * @return string
+     */
+    public static function serialize(array $array)
+    {
+        $raw = '';
+        $line = 0;
+        $keys = array_keys($array);
+        foreach ($keys as $key) {
+            $value = $array[$key];
+            $line++;
+
+            $raw .= $key . '|';
+
+            if (is_array($value) && isset($value['huge_recursion_blocker_we_hope'])) {
+                $raw .= 'R:' . $value['huge_recursion_blocker_we_hope'] . ';';
+            } else {
+                $raw .= serialize($value);
+            }
+            $array[$key] = Array('huge_recursion_blocker_we_hope' => $line);
+        }
+        return $raw;
+    }
+
+    public static function unserialize($session_data) {
+        $method = ini_get("session.serialize_handler");
+        switch ($method) {
+            case "php":
+                return self::unserialize_php($session_data);
+                break;
+            case "php_binary":
+                return self::unserialize_phpbinary($session_data);
+                break;
+            default:
+                throw new Exception("Unsupported session.serialize_handler: " . $method . ". Supported: php, php_binary");
+        }
+    }
+
+    private static function unserialize_php($session_data) {
+        $return_data = array();
+        $offset = 0;
+        while ($offset < strlen($session_data)) {
+            if (!strstr(substr($session_data, $offset), "|")) {
+                throw new Exception("invalid data, remaining: " . substr($session_data, $offset));
+            }
+            $pos = strpos($session_data, "|", $offset);
+            $num = $pos - $offset;
+            $varname = substr($session_data, $offset, $num);
+            $offset += $num + 1;
+            $data = unserialize(substr($session_data, $offset));
+            $return_data[$varname] = $data;
+            $offset += strlen(serialize($data));
+        }
+        return $return_data;
+    }
+
+    private static function unserialize_phpbinary($session_data) {
+        $return_data = array();
+        $offset = 0;
+        while ($offset < strlen($session_data)) {
+            $num = ord($session_data[$offset]);
+            $offset += 1;
+            $varname = substr($session_data, $offset, $num);
+            $offset += $num;
+            $data = unserialize(substr($session_data, $offset));
+            $return_data[$varname] = $data;
+            $offset += strlen(serialize($data));
+        }
+        return $return_data;
+    }
+}
